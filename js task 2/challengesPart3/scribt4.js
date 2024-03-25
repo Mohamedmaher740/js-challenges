1a547ffee01feaff0fb7c93d8bde40d4c4bba16b
Mohamedmaher740 <160148054+Mohamedmaher740@users.noreply.github.com>
2024-03-25 23:28:58 +0200

    Create scribt4.js

---
+++ js task 2/challengesPart3/scribt4.js
@@ -0,0 +1,21 @@
+const listOfNumbers =
+[
+    '01065881786',
+    '01249849775',
+    '01060714119',
+    '01577756455',
+    '01031575326',
+    '01225953081',
+    '01052488645',
+    '01032705664',
+    '01118805379',
+    '01042582352'
+]
+
+let i =0;
+let thePhoneNumber = listOfNumbers[i];
+
+for(i;i<listOfNumbers.length;i++)
+{
+    if(thePhoneNumber);
+}
